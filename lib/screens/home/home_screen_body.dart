66a12bb8b4658c01beb18968459e7e4268c50967
Emirhan <eKahya22@gmail.com>
2022-04-24 22:36:00 +0300

    24/04/2022 - 10:35

--- lib/screens/home/home_screen_body.dart
+++ lib/screens/home/home_screen_body.dart
@@ -4,6 +4,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gamekrub/models/special_deals_model.dart';
 import 'package:gamekrub/screens/search_screen/search_screen.dart';
 import 'package:gamekrub/utils/colors.dart';
+import 'package:gamekrub/widgets/mBigText.dart';
 import 'package:gamekrub/widgets/mSmallText.dart';
 import 'package:get/get.dart';
 
@@ -19,7 +20,6 @@ class HomeScreenBody extends StatefulWidget {
 }
 
 class _HomeScreenBodyState extends State<HomeScreenBody> {
-  late PageController _pageController = PageController(viewportFraction: 0.9);
   List img = ["banner1.png", "banner-2.jpg", "banner-3.jpg"];
 
   @override
@@ -27,7 +27,11 @@ class _HomeScreenBodyState extends State<HomeScreenBody> {
     return SingleChildScrollView(
       physics: BouncingScrollPhysics(),
       child: Container(
-        margin: EdgeInsets.only(top: Dimension.height40, left: Dimension.width20, right: Dimension.width20),
+        margin: EdgeInsets.only(
+          top: Dimension.height40,
+          left: Dimension.width20,
+          right: Dimension.width20,
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -35,7 +39,7 @@ class _HomeScreenBodyState extends State<HomeScreenBody> {
             SearchBarButton(
               press: () {
                 Get.to(
-                      () => SearchScreen(),
+                  () => SearchScreen(),
                   transition: Transition.rightToLeftWithFade,
                 );
               },
@@ -45,9 +49,7 @@ class _HomeScreenBodyState extends State<HomeScreenBody> {
               height: Dimension.width163,
               child: PageView.builder(
                 pageSnapping: true,
-                //onPageChanged: _onChanged,
                 itemCount: img.length,
-                controller: _pageController,
                 itemBuilder: (context, index) {
                   return _buildPageItem(index);
                 },
@@ -57,7 +59,7 @@ class _HomeScreenBodyState extends State<HomeScreenBody> {
             SizedBox(height: Dimension.height40),
             Container(
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   CategoryButton(
                       press: () {},
@@ -66,7 +68,7 @@ class _HomeScreenBodyState extends State<HomeScreenBody> {
                   CategoryButton(
                       press: () {},
                       text: "Marketplace",
-                      img: "assets/images/marketplace.png"),
+                      img: "assets/images/marketplace2.png"),
                   CategoryButton(
                       press: () {},
                       text: "Stream",
@@ -78,16 +80,16 @@ class _HomeScreenBodyState extends State<HomeScreenBody> {
                 ],
               ),
             ),
-            //Special Deals
+            //Popular Games
             SizedBox(height: Dimension.height40),
             Container(
-              margin: EdgeInsets.symmetric(horizontal: Dimension.width20),
+              margin: EdgeInsets.symmetric(horizontal: Dimension.width5),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  mSmallText(text: "Special Deals", color: AppColors.textColor),
+                  mSmallText(text: "Popular Games", color: AppColors.textColor),
                   GestureDetector(
-                    onTap: (){},
+                    onTap: () {},
                     child: Icon(
                       FontAwesomeIcons.angleRight,
                       size: Dimension.icon24,
@@ -99,23 +101,306 @@ class _HomeScreenBodyState extends State<HomeScreenBody> {
             ),
             SizedBox(height: Dimension.height20),
             Container(
-              height: 50,
-              color: Colors.red,
+              height: 95,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Dimension.radius15 / 2),
+              ),
               child: ListView.builder(
                 shrinkWrap: true,
                 itemCount: specialData.length,
                 scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index){
-                return Container(
-                  child: Row(
-                    children: [
-                      Text(specialData[index].name),
-                    ],
-                  ),
-                );
-              },
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.only(
+                        right: Dimension.width20, bottom: 5, top: 5),
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          BorderRadius.circular(Dimension.radius15 / 2),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.1),
+                          spreadRadius: 1,
+                          blurRadius: 1,
+                          offset: Offset(1, -1),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 95,
+                          width: 148,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              bottomLeft:
+                                  Radius.circular(Dimension.radius15 / 2),
+                              topLeft: Radius.circular(Dimension.radius15 / 2),
+                            ),
+                            color: Colors.red,
+                            image: DecorationImage(
+                              image: NetworkImage(specialData[index].img),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(
+                              top: Dimension.height10,
+                              left: Dimension.width10 * 1.5,
+                              bottom: Dimension.height10,
+                          ),
+                          height: 86,
+                          width: 170,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(Dimension.radius15 / 2),
+                              bottomRight:
+                                  Radius.circular(Dimension.radius15 / 2),
+                            ),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              mSmallText(
+                                text: specialData[index].name,
+                                color: AppColors.textColor,
+                                fontWeight: FontWeight.bold,
+                                fontSize: Dimension.fontSize30 / 2.3,
+                              ),
+                              mBigText(
+                                text: specialData[index].description,
+                                color: AppColors.textColor2,
+                                fontSize: Dimension.fontSize30 / 2.6,
+                              ),
+                              mBigText(
+                                text: "\$" + specialData[index].price.toString(),
+                                color: AppColors.textColor2,
+                                fontSize: Dimension.fontSize30 / 2,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
               ),
-            )
+            ),
+            //Popular Streaming
+            SizedBox(height: Dimension.height20),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: Dimension.width5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  mSmallText(text: "Popular Streaming", color: AppColors.textColor),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Icon(
+                      FontAwesomeIcons.angleRight,
+                      size: Dimension.icon24,
+                      color: AppColors.textColor2,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: Dimension.height20),
+            Container(
+              height: 95,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Dimension.radius15 / 2),
+              ),
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: specialData.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.only(
+                        right: Dimension.width20, bottom: 5, top: 5),
+                    decoration: BoxDecoration(
+                      borderRadius:
+                      BorderRadius.circular(Dimension.radius15 / 2),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.1),
+                          spreadRadius: 1,
+                          blurRadius: 1,
+                          offset: Offset(1, -1),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 95,
+                          width: 148,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              bottomLeft:
+                              Radius.circular(Dimension.radius15 / 2),
+                              topLeft: Radius.circular(Dimension.radius15 / 2),
+                            ),
+                            color: Colors.red,
+                            image: DecorationImage(
+                              image: NetworkImage(specialData[index].img),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(
+                            top: Dimension.height10,
+                            left: Dimension.width10 * 1.5,
+                            bottom: Dimension.height10,
+                          ),
+                          height: 86,
+                          width: 170,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(Dimension.radius15 / 2),
+                              bottomRight:
+                              Radius.circular(Dimension.radius15 / 2),
+                            ),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              mSmallText(
+                                text: specialData[index].name,
+                                color: AppColors.textColor,
+                                fontWeight: FontWeight.bold,
+                                fontSize: Dimension.fontSize30 / 2.3,
+                              ),
+                              mBigText(
+                                text: specialData[index].description,
+                                color: AppColors.textColor2,
+                                fontSize: Dimension.fontSize30 / 2.6,
+                              ),
+                              mBigText(
+                                text: "\$" + specialData[index].price.toString(),
+                                color: AppColors.textColor2,
+                                fontSize: Dimension.fontSize30 / 2,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            //Top Selling Games
+            SizedBox(height: Dimension.height20),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: Dimension.width5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  mSmallText(text: "Top Selling Games", color: AppColors.textColor),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Icon(
+                      FontAwesomeIcons.angleRight,
+                      size: Dimension.icon24,
+                      color: AppColors.textColor2,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: Dimension.height20),
+            Container(
+              height: 95,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Dimension.radius15 / 2),
+              ),
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: specialData.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.only(
+                        right: Dimension.width20, bottom: 5, top: 5),
+                    decoration: BoxDecoration(
+                      borderRadius:
+                      BorderRadius.circular(Dimension.radius15 / 2),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.1),
+                          spreadRadius: 1,
+                          blurRadius: 1,
+                          offset: Offset(1, -1),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 95,
+                          width: 148,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              bottomLeft:
+                              Radius.circular(Dimension.radius15 / 2),
+                              topLeft: Radius.circular(Dimension.radius15 / 2),
+                            ),
+                            color: Colors.red,
+                            image: DecorationImage(
+                              image: NetworkImage(specialData[index].img),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(
+                            top: Dimension.height10,
+                            left: Dimension.width10 * 1.5,
+                            bottom: Dimension.height10,
+                          ),
+                          height: 86,
+                          width: 170,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(Dimension.radius15 / 2),
+                              bottomRight:
+                              Radius.circular(Dimension.radius15 / 2),
+                            ),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              mSmallText(
+                                text: specialData[index].name,
+                                color: AppColors.textColor,
+                                fontWeight: FontWeight.bold,
+                                fontSize: Dimension.fontSize30 / 2.3,
+                              ),
+                              mBigText(
+                                text: specialData[index].description,
+                                color: AppColors.textColor2,
+                                fontSize: Dimension.fontSize30 / 2.6,
+                              ),
+                              mBigText(
+                                text: "\$" + specialData[index].price.toString(),
+                                color: AppColors.textColor2,
+                                fontSize: Dimension.fontSize30 / 2,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
